d09a955cea4f8e02297dce1c8128aa94de5ceb01
Orji Favour <favour@titanname.com>
2023-05-14 11:17:25 -0700

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -11,11 +11,23 @@ from datetime import datetime
 class BaseModel:
     """Basemode class defines all our essetial attributes and method"""
 
-    def __init__(self):
-        """this is a constructor of our basemodel class"""
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """this is a constructor of our basemodel class
+           *args not used
+           **kwargs is a dict
+        """
+        if kwargs is not None and kwargs != {}:
+            for k in kwargs.item():
+                if k == "created_at":
+                    self.__dict__["craeted_at"] = datetime.strptime(kwargs["craeted_at"], '%d/%m/%y %H:%M:%S')
+                elif k == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(kwargs["updated_at"], '%d/%m/%y %H:%M:%S')
+                else:
+                    self.__dict__[k] = kwargs[k]
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """return a valid string representation of our instance"""
