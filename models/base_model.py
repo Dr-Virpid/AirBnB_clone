20e28e584bad961728aa1bad6b31a5f4aa64dcb9
Orji Favour <favour@titanname.com>
2023-05-15 05:14:23 -0700

    mistake

--- models/base_model.py
+++ models/base_model.py
@@ -3,9 +3,9 @@
 Write a class BaseModel that defines all
 common attributes/methods for other classes
 """
-
 from uuid import uuid4
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -19,24 +19,29 @@ class BaseModel:
         if kwargs is not None and kwargs != {}:
             for k in kwargs.item():
                 if k == "created_at":
-                    self.__dict__["craeted_at"] = datetime.strptime(kwargs["craeted_at"], '%d/%m/%y %H:%M:%S')
+                    self.__dict__["craeted_at"] = datetime.\
+                        strptime(kwargs["created_at"], '%d/%m/%y %H:%M:%S')
                 elif k == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(kwargs["updated_at"], '%d/%m/%y %H:%M:%S')
+                    self.__dict__["updated_at"] = datetime.\
+                        strptime(kwargs["updated_at"], '%d/%m/%y %H:%M:%S')
                 else:
                     self.__dict__[k] = kwargs[k]
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """return a valid string representation of our instance"""
-        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+        return "[{}] ({}) {}".\
+               format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """updates the public instance
         attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """returns a dictionary containing
